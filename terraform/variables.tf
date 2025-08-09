0ded000e16b7aedbe0bedc7217e05e6f21070e44
Basia Adamiec <badamiec3@live.com>
2025-08-09 23:28:25 +0100

    Conceal secrets manager path

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,5 +7,6 @@ variable "project_name" {
 variable "github_token_path" {
   description = "GitHub token for accessing private repositories"
   type        = string
-  default     = "arn:aws:secretsmanager:eu-west-1:560460596452:secret:github/pat/hcp-packer-builder-ngQziq"
+  sensitive   = true
+  default     = ""
 }

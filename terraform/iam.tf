3fb1fd4450f228ffc556ff1c2237ad07e4cb6b8f
Basia Adamiec <badamiec3@live.com>
2025-08-09 22:31:47 +0100

    Attempt after trust updated

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,7 +2,6 @@ resource "aws_iam_role" "github_codebuild_runner_role" {
   name                = "${var.project_name}-role"
   assume_role_policy  = data.aws_iam_policy_document.github_codebuild_runner_role_trust_policy.json
   managed_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
-
 }
 
 data "aws_iam_policy_document" "github_codebuild_runner_role_trust_policy" {

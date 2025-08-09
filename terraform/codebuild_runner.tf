44e1982380e75461bc0491176ffe98a8bc4ad913
Basia Adamiec <badamiec3@live.com>
2025-08-09 23:32:18 +0100

    Upgrade runner to medium

--- terraform/codebuild_runner.tf
+++ terraform/codebuild_runner.tf
@@ -12,7 +12,7 @@ resource "aws_codebuild_project" "packer_builder" {
   }
 
   environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
+    compute_type = "BUILD_GENERAL1_MEDIUM"
     image        = "aws/codebuild/amazonlinux2-x86_64-standard:6.0"
     type         = "LINUX_CONTAINER"
     #image_pull_credentials_type = "CODEBUILD"
